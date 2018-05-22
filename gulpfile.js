796205f57fcc43643ec02bc09709449b8799e072
Ray Villalobos <ray@planetoftheweb.com>
2018-05-22 19:02:46 -0400

    slideshow loaded into gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,8 @@
-const
-  gulp = require('gulp'),
+const gulp = require('gulp'),
   webserver = require('gulp-webserver'),
-  babel = require('gulp-babel'),
-  sourcemaps = require('gulp-sourcemaps'),
 
-  source = 'process/',
-  dest = 'builds/codeclinic/';
+  source = './',
+  dest = './';
 
 gulp.task('html', function() {
   gulp.src(dest + '**/*.html');
@@ -16,28 +13,19 @@ gulp.task('css', function() {
   gulp.src(dest + '**/*.css');
 });
 
-// JavaScript ES6
-gulp.task('js', function() {
-  gulp.src(source + 'js/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(dest + 'js'));
-});
-
 gulp.task('watch', function() {
   gulp.watch(source + '**/*.js', ['js']);
-  gulp.watch(dest + '**/*.css', ['css']); //CSS
-  gulp.watch(dest + '**/*.html', ['html']);
+  gulp.watch(source + '**/*.css', ['css']); //CSS
+  gulp.watch(source + '**/*.html', ['html']);
 });
 
 gulp.task('webserver', function() {
   gulp.src(dest)
     .pipe(webserver({
       livereload: true,
-      port: 3000,
+      port: 3333,
       open: true
     }));    
 });
 
-gulp.task('default', ['html', 'css', 'js', 'webserver', 'watch']);
+gulp.task('default', ['webserver','watch']);
