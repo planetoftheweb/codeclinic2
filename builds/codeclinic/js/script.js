b3618bb24e7985bdeea46d9e004e523ad83d5352
Ray Villalobos <ray@planetoftheweb.com>
2018-05-27 17:19:06 -0400

    dashboard target

--- builds/codeclinic/js/script.js
+++ builds/codeclinic/js/script.js
@@ -1,147 +1,80 @@
-$(function () {
-
-  'use strict';
-
-  var fromDate, toDate;
-  var myDate = '2005-12-31';
-
-  function getMean(myArray) {
-    var mean = myArray.reduce(function (a, b) {
-      return a + b;
-    }) / myArray.length;
-    return mean.toFixed(2);
-  } //getMean
-
-  function getMedian(myArray) {
-    var median;
-    var sorted = myArray.sort(function (a, b) {
-      return a > b;
-    });
-    var middleIndex = Math.floor(sorted.length / 2);
-
-    if (sorted.length % 2 === 0) {
-      var medianA = sorted[middleIndex];
-      var medianB = sorted[middleIndex - 1];
-      median = (medianA + medianB) / 2;
-    } else {
-      median = sorted[middleIndex];
+$(function() {
+  data_red = ['red'];
+  data_green = ['green'];
+  data_blue = ['blue'];
+  var chart = c3.generate({
+    data: {
+      columns: [['red'], ['green'], ['blue']]
+    },
+    axis: {
+      y: {
+        max: 300,
+        min: 0
+      }
+    },
+    color: {
+      pattern: ['#dc3545', '#28a745', '#007bff'] // the three color levels for the percentage values.
     }
+  });
 
-    return median.toFixed(2);
-  } //getMedian
-
-
-  function processData(data) {
-    var myData = [];
-
-    var myDates = ['x'];
-    var meanTemps = ['Mean Temperature'];
-    var medTemps = ['Median Temperature'];
-    var meanPress = ['Mean Pressure'];
-    var medPress = ['Median Pressure'];
-    var meanSpeeds = ['Mean Speed'];
-    var medSpeeds = ['Median Speed'];
-
-    for (var key in data) {
-      if (data.hasOwnProperty(key)) {
-        if (data[key].t !== null && data[key].p !== null && data[key].s !== null) {
-          myDates.push(key);
-          meanTemps.push(getMean(data[key].t));
-          medTemps.push(getMedian(data[key].t));
-          meanPress.push(getMean(data[key].p));
-          medPress.push(getMedian(data[key].p));
-          meanSpeeds.push(getMean(data[key].s));
-          medSpeeds.push(getMedian(data[key].s));
-        } //data is not null
-      } // hasOwnProperty
-    } // for key in data
-
-    myData.push(myDates, meanTemps, medTemps, meanPress, medSpeeds, meanSpeeds);
-    return myData;
-  } // Process Data
-
-  function generateChart(data) {
-    var chart = c3.generate({
-
-      data: {
-        x: 'x',
-        columns: data,
-        type: 'bar',
-        groups: [['Mean Temperature', 'Median Temperature', 'Mean Pressure', 'Median Pressure', 'Mean Speed', 'Median Speed']]
-      },
-      bar: {
-        width: {
-          ratio: 0.9
-        }
-      },
-      axis: {
-        x: {
-          type: 'timeseries',
-          tick: {
-            format: '%Y-%m-%d' // x
-          } } }, // axis
-      subchart: {
-        show: true //subchart
-      } }); // chart
-  } // generateChart
-
-
-  function loadChart() {
+  setInterval(function() {
     $.ajax({
-      url: 'http://foundationphp.com/phpclinic/podata.php?&raw&callback=?',
-      jsonpCallback: 'jsonReturnData',
-      dataType: 'jsonp',
-      data: {
-        startDate: formatDate(fromDate, ''),
-        endDate: formatDate(toDate, ''),
-        format: 'json'
-      },
-      success: function (response) {
-        generateChart(processData(response));
+      url: 'http://pixelprowess.com/i/stream.php',
+      dataType: 'json',
+      success: function(data) {
+        $('#time-stamp').text(data.time_stamp);
+
+        $('#progress-pulsometer').css(
+          'width',
+          (data.pulsometer_readout - 1) / 255 * 100 + '%'
+        );
+        $('#progress-pulsometer').text(data.pulsometer_readout);
+
+        $('#progress-efficiency').css(
+          'width',
+          data.engine_efficiency / 100 * 100 + '%'
+        );
+        $('#progress-efficiency').text(data.engine_efficiency);
+
+        $('#progress-red').css('width', data.red_value / 255 * 100 + '%');
+        $('#progress-red').text(data.red_value);
+        $('#progress-green').css('width', data.green_value / 255 * 100 + '%');
+        $('#progress-green').text(data.green_value);
+        $('#progress-blue').css('width', data.blue_value / 255 * 100 + '%');
+        $('#progress-blue').text(data.blue_value);
+        $('#current-color').css(
+          'backgroundColor',
+          'rgb(' +
+            data.red_value +
+            ',' +
+            data.green_value +
+            ',' +
+            data.blue_value +
+            ')'
+        );
+        if (data_red.length > 10) {
+          data_red.splice(1, 1);
+          data_green.splice(1, 1);
+          data_blue.splice(1, 1);
+        }
+        console.log(data);
+
+        data_red.push(data.red_value);
+        data_green.push(data.green_value);
+        data_blue.push(data.blue_value);
+        chart.load({
+          columns: [data_red, data_green, data_blue]
+        });
+
+        // aria - valuenow="25"
       } //success
-
     }); //AJAX Call
-  } //load Chart
-
-  function formatDate(date, divider) {
-    var someday = new Date(date);
-    var month = someday.getUTCMonth() + 1;
-    var day = someday.getUTCDate();
-    var year = someday.getUTCFullYear();
-
-    if (month <= 9) {
-      month = '0' + month;
-    }
-    if (day <= 9) {
-      day = '0' + day;
-    }
-
-    return '' + year + divider + month + divider + day;
-  }
-
-  //set up
-
-  fromDate = new Date(myDate);
-  fromDate.setDate(fromDate.getDate() - 31);
-
-  toDate = new Date(myDate);
-  toDate.setDate(toDate.getDate() - 1);
-
-  document.forms.rangeform.from.value = formatDate(fromDate, '-');
-  document.forms.rangeform.to.value = formatDate(toDate, '-');
-
-  loadChart();
-
-  // Events -------
-
-  document.forms.rangeform.addEventListener('change', function (e) {
-    fromDate = new Date(document.rangeform.from.value);
-    toDate = new Date(document.rangeform.to.value);
-
-    fromDate = fromDate.toUTCString();
-    toDate = toDate.toUTCString();
-
-    loadChart();
-  }, false);
+  }, 1000);
 }); // Page Loaded
-//# sourceMappingURL=script.js.map
+
+// time_stamp
+// pulsometer_readout
+// engine_efficiency
+// red_value
+// green_value
+// blue_value
