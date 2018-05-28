08a52e8b702fe7137b842367a1ff34f34c60a23f
Ray Villalobos <ray@planetoftheweb.com>
2018-05-28 17:14:55 -0400

    scatter plot coefficient

--- builds/codeclinic/js/script.js
+++ builds/codeclinic/js/script.js
@@ -1,147 +1,84 @@
-$(function () {
-
-  'use strict';
-
-  var fromDate, toDate;
-  var myDate = '2005-12-31';
-
-  function getMean(myArray) {
-    var mean = myArray.reduce(function (a, b) {
-      return a + b;
-    }) / myArray.length;
-    return mean.toFixed(2);
-  } //getMean
-
-  function getMedian(myArray) {
-    var median;
-    var sorted = myArray.sort(function (a, b) {
-      return a > b;
-    });
-    var middleIndex = Math.floor(sorted.length / 2);
-
-    if (sorted.length % 2 === 0) {
-      var medianA = sorted[middleIndex];
-      var medianB = sorted[middleIndex - 1];
-      median = (medianA + medianB) / 2;
-    } else {
-      median = sorted[middleIndex];
-    }
-
-    return median.toFixed(2);
-  } //getMedian
-
-
-  function processData(data) {
-    var myData = [];
-
-    var myDates = ['x'];
-    var meanTemps = ['Mean Temperature'];
-    var medTemps = ['Median Temperature'];
-    var meanPress = ['Mean Pressure'];
-    var medPress = ['Median Pressure'];
-    var meanSpeeds = ['Mean Speed'];
-    var medSpeeds = ['Median Speed'];
-
-    for (var key in data) {
-      if (data.hasOwnProperty(key)) {
-        if (data[key].t !== null && data[key].p !== null && data[key].s !== null) {
-          myDates.push(key);
-          meanTemps.push(getMean(data[key].t));
-          medTemps.push(getMedian(data[key].t));
-          meanPress.push(getMean(data[key].p));
-          medPress.push(getMedian(data[key].p));
-          meanSpeeds.push(getMean(data[key].s));
-          medSpeeds.push(getMedian(data[key].s));
-        } //data is not null
-      } // hasOwnProperty
-    } // for key in data
-
-    myData.push(myDates, meanTemps, medTemps, meanPress, medSpeeds, meanSpeeds);
-    return myData;
-  } // Process Data
-
+$(function() {
   function generateChart(data) {
     var chart = c3.generate({
-
       data: {
-        x: 'x',
-        columns: data,
-        type: 'bar',
-        groups: [['Mean Temperature', 'Median Temperature', 'Mean Pressure', 'Median Pressure', 'Mean Speed', 'Median Speed']]
-      },
-      bar: {
-        width: {
-          ratio: 0.9
+        y: 'barometric_pressure',
+        x: 'keys',
+        xFormat: '%Y-%m-%d %H:%M:%S',
+        json: data,
+        type: 'spline',
+        types: {
+          barometric_pressure: 'scatter',
+          average: 'line',
+          delta: 'line'
         }
+      },
+      point: {
+        show: false
       },
       axis: {
         x: {
           type: 'timeseries',
           tick: {
-            format: '%Y-%m-%d' // x
-          } } }, // axis
+            count: 4,
+            format: '%Y-%m-%d %H:%M:%S'
+          }
+        }
+      },
       subchart: {
         show: true //subchart
-      } }); // chart
+      }
+    }); // chart
   } // generateChart
-
 
   function loadChart() {
     $.ajax({
-      url: 'http://foundationphp.com/phpclinic/podata.php?&raw&callback=?',
-      jsonpCallback: 'jsonReturnData',
-      dataType: 'jsonp',
+      url: 'http://pixelprowess.com/i/lake.php',
+      dataType: 'json',
       data: {
-        startDate: formatDate(fromDate, ''),
-        endDate: formatDate(toDate, ''),
+        // startDate: formatDate(fromDate, ''),
+        // endDate: formatDate(toDate, ''),
         format: 'json'
       },
-      success: function (response) {
-        generateChart(processData(response));
-      } //success
+      success: function(response) {
+        var sum = response.barometric_pressure.reduce((a, b) => a + b, 0);
+        var size = response.barometric_pressure.length;
+        var avg = sum / size;
+        var begx = (response.barometric_pressure[0] + avg) / 2;
+        var endx = (response.barometric_pressure[size - 1] + avg) / 2;
+        var slope = (endx - begx) / size;
 
+        // response.average = response.barometric_pressure.map(function(x) {
+        //   return avg;
+        // });
+
+        // response.delta = response.barometric_pressure.map(function(x) {
+        //   return (x + avg) / 2;
+        // });
+
+        response.coefficient = response.barometric_pressure.map(function(x, i) {
+          return begx + i * slope;
+        });
+
+        generateChart(response);
+      } //success
     }); //AJAX Call
   } //load Chart
 
-  function formatDate(date, divider) {
-    var someday = new Date(date);
-    var month = someday.getUTCMonth() + 1;
-    var day = someday.getUTCDate();
-    var year = someday.getUTCFullYear();
-
-    if (month <= 9) {
-      month = '0' + month;
-    }
-    if (day <= 9) {
-      day = '0' + day;
-    }
-
-    return '' + year + divider + month + divider + day;
-  }
-
-  //set up
-
-  fromDate = new Date(myDate);
-  fromDate.setDate(fromDate.getDate() - 31);
-
-  toDate = new Date(myDate);
-  toDate.setDate(toDate.getDate() - 1);
-
-  document.forms.rangeform.from.value = formatDate(fromDate, '-');
-  document.forms.rangeform.to.value = formatDate(toDate, '-');
-
   loadChart();
 
-  // Events -------
+  document.forms.rangeform.addEventListener(
+    'change',
+    function(e) {
+      fromDate = new Date(document.rangeform.from.value);
+      toDate = new Date(document.rangeform.to.value);
 
-  document.forms.rangeform.addEventListener('change', function (e) {
-    fromDate = new Date(document.rangeform.from.value);
-    toDate = new Date(document.rangeform.to.value);
+      fromDate = fromDate.toUTCString();
+      toDate = toDate.toUTCString();
 
-    fromDate = fromDate.toUTCString();
-    toDate = toDate.toUTCString();
-
-    loadChart();
-  }, false);
+      loadChart();
+    },
+    false
+  );
 }); // Page Loaded
 //# sourceMappingURL=script.js.map
