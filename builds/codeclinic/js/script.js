c8dbe8a007f345e1f063d899cb2e0f15df2b75ce
Ray Villalobos <ray@planetoftheweb.com>
2018-05-29 18:00:03 -0700

    finished 01_05e

--- builds/codeclinic/js/script.js
+++ builds/codeclinic/js/script.js
@@ -1,7 +1,17 @@
 (function() {
+  let fromDate = '2015-01-01';
+  let fromTime = '01:00:00';
+
+  let toDate = '2015-01-01';
+  let toTime = '10:00:00';
+
   function loadChart() {
     fetch('http://pixelprowess.com/i/lake.php', {
-      method: 'POST'
+      method: 'POST',
+      body: JSON.stringify({
+        fromDate: fromDate + ' ' + fromTime,
+        toDate: toDate + ' ' + toTime
+      })
     })
       .then(response => response.json())
       .then(response => {
