c824751f285a8d431ace841fc2ff83488a4912dd
Ray Villalobos <ray@planetoftheweb.com>
2018-05-31 18:04:32 -0700

    matched target

--- builds/codeclinic/js/script.js
+++ builds/codeclinic/js/script.js
@@ -16,6 +16,30 @@
   let solutionsQty;
   let currentSolution = 0;
 
+  function Board() {
+    this.width = numColumns;
+    this.lastRow = this.width - 1;
+    this.columns = new Array(this.width);
+
+    let numberOfDiagonals = 2 * this.width - 1;
+    this.diagDown = new Array(numberOfDiagonals);
+    this.diagUp = new Array(numberOfDiagonals);
+    this.solutions = [];
+
+    for (
+      let index = 0;
+      index < numberOfDiagonals;
+      ++index
+    ) {
+      if (index < this.width) {
+        this.columns[index] = -1;
+      }
+      this.diagDown[index] = free;
+      this.diagUp[index] = free;
+    }
+    this.position.numColumns;
+  }
+
   document
     .querySelector('#Board')
     .addEventListener(
