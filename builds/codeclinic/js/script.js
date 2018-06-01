fbfe6c1d136b07e5b591c64f2318b9d33a960f96
Ray Villalobos <ray@planetoftheweb.com>
2018-05-31 17:51:08 -0700

    finished setting up

---
+++ builds/codeclinic/js/script.js
@@ -0,0 +1,38 @@
+(function() {
+  const occupied = 1;
+  const free = 0;
+  let columnNames = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H'
+  ];
+  let numColumns = 8;
+  let allSolutions = [];
+  let solutionsQty;
+  let currentSolution = 0;
+
+  document
+    .querySelector('#Board')
+    .addEventListener(
+      'click',
+      e => {
+        if (e.target.tagName === 'path') {
+          if (
+            e.target.style.fill ===
+            'rgb(211, 54, 130)'
+          ) {
+            e.target.style.fill = 'transparent';
+          } else {
+            e.target.style.fill =
+              'rgb(211, 54, 130)';
+          }
+        }
+      },
+      false
+    );
+})(); // IIFE
