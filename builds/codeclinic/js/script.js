07d854c81a083111866eee44363b03e1fa1e22b2
Ray Villalobos <ray@planetoftheweb.com>
2018-06-02 09:22:40 -0700

    finished

--- builds/codeclinic/js/script.js
+++ builds/codeclinic/js/script.js
@@ -3,12 +3,47 @@
     myOscillator,
     myGain,
     myDistortion,
-    myOscillator;
+    myOscillator,
+    originalYPos,
+    originalXPos,
+    originalFrequency,
+    scaleFrequencies = [
+      110,
+      123.47,
+      130.81,
+      146.83,
+      164.81,
+      174.61,
+      196,
+      220,
+      246.94,
+      261.63,
+      293.66,
+      329.63,
+      349.23,
+      392,
+      440,
+      493.88,
+      523.25,
+      587.33,
+      659.25,
+      698.46,
+      783.99,
+      880,
+      987.77,
+      1046.5,
+      1174.66,
+      1318.51,
+      1396.91,
+      1567.98,
+      1760
+    ];
 
   var contextClass =
     window.AudioContext || window.webkitAudioContext;
 
   var appNode = document.querySelector('.app');
+  var appWidth = appNode.offsetWidth;
 
   if (contextClass) {
     mySoundContext = new contextClass();
@@ -22,9 +57,20 @@
       myOscillator.stop();
     }
 
+    mouseXPos = e.clientX;
+    mouseYPos = e.clientY;
+    originalYPos = mouseYPos;
+
+    originalFrequency =
+      scaleFrequencies[
+        Math.floor(
+          mouseXPos / appWidth * scaleFrequencies.length
+        )
+      ];
+
     myOscillator = mySoundContext.createOscillator();
     myOscillator.type = 'sine';
-    myOscillator.frequency.value = 110;
+    myOscillator.frequency.value = originalFrequency;
     myOscillator.start();
 
     myGain = mySoundContext.createGain();
