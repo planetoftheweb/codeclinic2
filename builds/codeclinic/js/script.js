71302c2e9d8aaf0ce13a376fb56a180a5dd149cb
Ray Villalobos <ray@planetoftheweb.com>
2018-06-04 15:34:59 -0700

    finished

--- builds/codeclinic/js/script.js
+++ builds/codeclinic/js/script.js
@@ -27,7 +27,40 @@ $(function() {
         droppedImage.src = e.target.result;
         droppedImage.className = 'picture';
         target.html(droppedImage);
+        detectImageFaces();
       };
       fileReader.readAsDataURL(file);
     });
+
+  function detectImageFaces() {
+    if (droppedImage !== undefined) {
+      $('.face').remove();
+      $('.warning').remove();
+      $('.picture').faceDetection({
+        complete: function(faces) {
+          for (let i = 0; i < faces.length; i++) {
+            $('<div>', {
+              class: 'face',
+              css: {
+                position: 'absolute',
+                left: faces[i].x * faces[i].scaleX + 'px',
+                top: faces[i].y * faces[i].scaleY + 'px',
+                width:
+                  faces[i].width * faces[i].scaleX + 'px',
+                height:
+                  faces[i].height * faces[i].scaleY + 'px'
+              } // css
+            }).insertAfter(this); // div
+          } //loop through faces
+        } // complete
+      }); // face detection
+    } else {
+      var warningMsg = $('.warning').html(
+        '<p class="alert alert-danger">You must drop an image to analyze.</p>'
+      );
+    } // undefined image
+  } // detectImageFaces
+
+  $('#analyze').click(detectImageFaces);
+  $(window).resize(detectImageFaces);
 });
