cf1962c3a128cf8b9da1182980778d66d8f0bad5
Ray Villalobos <ray@planetoftheweb.com>
2018-06-04 17:39:51 -0700

    finished

--- builds/codeclinic/js/script.js
+++ builds/codeclinic/js/script.js
@@ -6,6 +6,15 @@ $(function() {
   var chart = c3.generate({
     data: {
       columns: [['red'], ['green'], ['blue']]
+    },
+    axis: {
+      y: {
+        max: 300,
+        min: 0
+      }
+    },
+    color: {
+      pattern: ['#dc3545', '#28a745', '#007bff']
     }
   });
 
@@ -40,9 +49,35 @@ $(function() {
             data.blue_value +
             ')'
         );
+
+        $('#time-stamp').text(data.time_stamp);
+
+        $('#progress-pulsometer').css(
+          'width',
+          (data.pulsometer_readout - 1) / 255 * 100 + '%'
+        );
+        $('#progress-pulsometer').text(
+          data.pulsometer_readout
+        );
+
+        $('#progress-efficiency').css(
+          'width',
+          data.engine_efficiency / 100 * 100 + '%'
+        );
+        $('#progress-efficiency').text(
+          data.engine_efficiency
+        );
+
+        if (data_red.length > 10) {
+          data_red.splice(1, 1);
+          data_green.splice(1, 1);
+          data_blue.splice(1, 1);
+        }
+
         data_red.push(data.red_value);
         data_green.push(data.green_value);
         data_blue.push(data.blue_value);
+
         chart.load({
           columns: [data_red, data_green, data_blue]
         }); //load
